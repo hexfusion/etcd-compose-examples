d16944cc5f3abecfe55086ec16c5321af7f4a72b
Sam Batschelet <samb@endpoint.com>
2017-12-18 21:52:32 -0500

    Add data population script.

---
+++ etcd2-prometheus/data_pop.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+etcdport=`docker port etcd2prometheus_etcd1_1 | grep 2379 | cut -f2- -d:`
+promport=`docker port etcd2prometheus_prom_1 | cut -f2- -d:`
+
+function valid {
+    for i in `seq 1 1000`; do
+	    curl http://127.0.0.1:${etcdport}/v2/keys/$i
+	done
+}
+
+function invalid {
+    for i in `seq 1 1000`; do
+	    curl http://127.0.0.1:${etcdport}/v2/keys/$1 -XGET -d value=bar -v
+	done
+}
+
+valid
+invalid
+
+echo "View results here http://127.0.0.1:$promport"
