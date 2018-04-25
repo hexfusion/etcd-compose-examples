84af226a189373ddc5550a543b19e1523e14487f
Sam Batschelet <sam@hexfusion.com>
2018-04-24 22:48:55 -0400

    Add full TLS discover example

---
+++ discovery/dns-wildcard/etcdctl_test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+etcdctl --cacert certs/ca.pem \
+    --cert certs/client.pem \
+	--key certs/client-key.pem \
+	--endpoints=https://172.16.8.219:2379 \
+    --insecure-skip-tls-verify=true member list
